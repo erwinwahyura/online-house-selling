c55c9be279be852831aa86c049c70343f2d1d0c9
erwinwahyura <you@example.com>
2017-06-13 13:09:19 +0700

    routing api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express')
 var router = express.Router()
+var c_house = require('../controllers/c_house')
 
 router.get('/', function(req, res) {
   res.send('alive')
 })
+
+router.post('/api/house', c_house.add)
+router.get('/api/house', c_house.getAll) //done get user
+router.get('/api/house/:_id', c_house.getById) //done
+router.put('/api/house/:_id', c_house.edit) //done edit user hash password
+router.delete('/api/house/:_id', c_house.remove) //done
 
 module.exports = router
